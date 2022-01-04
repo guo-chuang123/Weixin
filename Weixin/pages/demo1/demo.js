6b676128f9c197fa8ec88460553028aaf67c7f73
Oscar <2547239232@qq.com>
2022-01-04 11:57:56 +0800

    1

--- Weixin/pages/demo1/demo.js
+++ Weixin/pages/demo1/demo.js
@@ -1,66 +1,43 @@
-// pages/demo1/demo.js
 Page({
-
+ 
   /**
    * 页面的初始数据
    */
   data: {
-
+    msg: 'Oscar',
+    person: {
+      name: 'Peter',
+      age: 18,
+      sex:"男"
+    },
+    list: [
+      {
+        id: 0,
+        name:"元吉"
+      },
+      {
+        id: 1,
+        name:"元芳"
+      },
+      {
+        id: 2,
+        name:"元也"
+      },
+      {
+        id: 3,
+        name:"元文"
+      }
+    ],
+    isCheck: true,
+    num:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  add(e) {
+    this.setData({num:e.detail.value})
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handle(e) {
+    let targ = e.currentTarget.dataset.operation
+    this.setData({
+      num:this.data.num + targ
+    })
   }
 })
