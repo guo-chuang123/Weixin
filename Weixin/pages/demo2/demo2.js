3ecc99b2d16c57e1ec3b9ca62bfb574888e6472a
Oscar <2547239232@qq.com>
2022-01-05 16:03:54 +0800

    2

---
+++ Weixin/pages/demo2/demo2.js
@@ -0,0 +1,69 @@
+// pages/demo2/demo2.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    html:'<div class="goods-others"><a href="//red.jd.com/?brandId=248742" target="_blank" clstag="h|keycount|core|spark_c01" class="other-item"><div class="other-item__logo"><img src="//img11.360buyimg.com/seckillcms/s600x300_jfs/t1/214786/12/9433/4150/61cbda69Ea78c5176/27e5e8cee83cce8a.png!cc_300x150.webp" alt="ERDOS1月4日闪购"><div class="logo-overlap"><span>闪购专场，限时直降</span></div></div><h6 class="other-item__name">ERDOS1月4日闪购</h6></a><a href="//red.jd.com/?brandId=248761" target="_blank" clstag="h|keycount|core|spark_c02" class="other-item"><div class="other-item__logo"><img src="//img10.360buyimg.com/seckillcms/s600x300_jfs/t1/126845/18/20727/15601/61cd1f93Ea3283cdd/77fe2e66ed58b7dc.jpg!cc_300x150.webp" alt="飞利浦闪购活动"><div class="logo-overlap"><span>闪购促销，限时优惠</span></div></div><h6 class="other-item__name">飞利浦闪购活动</h6></a><a href="//red.jd.com/?brandId=248952" target="_blank" clstag="h|keycount|core|spark_c03" class="other-item"><div class="other-item__logo"><img src="//img14.360buyimg.com/seckillcms/s600x300_jfs/t1/164072/9/24927/22958/61ceb912E15b6f8a6/efea0f60e684d7e3.png!cc_300x150.webp" alt="添柏岚品牌专场"><div class="logo-overlap"><span>叠券满1290减290</span></div></div><h6 class="other-item__name">添柏岚品牌专场</h6></a><a href="//red.jd.com/?brandId=248864" target="_blank" clstag="h|keycount|core|spark_c04" class="other-item"><div class="other-item__logo"><img src="//img20.360buyimg.com/seckillcms/s600x300_jfs/t1/93838/27/20138/14112/61ce9ab4Eb169d7e3/555ffa49d31c9480.png!cc_300x150.webp" alt="六福珠宝闪购专场"><div class="logo-overlap"><span>爆款每滿200減20</span></div></div><h6 class="other-item__name">六福珠宝闪购专场</h6></a><a href="//red.jd.com/?brandId=248843" target="_blank" clstag="h|keycount|core|spark_c05" class="other-item"><div class="other-item__logo"><img src="//img13.360buyimg.com/seckillcms/s600x300_jfs/t1/168802/27/28708/9092/61cea44fE549b5037/ec97a59a6b751c2e.png!cc_300x150.webp" alt="韩都衣舍闪购专场"><div class="logo-overlap"><span>秋冬爆款低至4折</span></div></div><h6 class="other-item__name">韩都衣舍闪购专场</h6></a><a href="//red.jd.com/?brandId=249224" target="_blank" clstag="h|keycount|core|spark_c06" class="other-item"><div class="other-item__logo"><img src="//img10.360buyimg.com/seckillcms/s600x300_jfs/t1/97797/33/19713/27211/61cea0d1Eadf27925/6f6fcba82f2a9c03.png!cc_300x150.webp" alt="五金工具大牌闪购"><div class="logo-overlap"><span>爆品直降</span></div></div><h6 class="other-item__name">五金工具大牌闪购</h6></a></div>'
+  },
+  getUserinfo(e) {
+    console.log(e);
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
